dd0351da0326a8cf41166655ff68800f3d801698
jgrozell <joseph.rozell11@gmail.com>
2023-08-13 22:36:46 -0700

    update

--- PlotRandData.py
+++ PlotRandData.py
@@ -1,16 +1,19 @@
 import random
 import matplotlib as mat
+
 import matplotlib.pyplot as plt
-import Point, LineString, Polygon
+import Point
 
 # creating the data set
-dataSet = []
+dataSet = [Point(1,1)]
 
 num = int(input("How many data points should there be?"))
 
+dataSet.append(Point(1,1))
+
 for i in range(num):
-    dataSet.append(Point(random.random(), random.random()))
-    
+    #dataSet.append(Point(i,i))
+    i = i+1
     #dataSet[0].append(random.random())
     #dataSet[1].append(random.random())
 
--- Point.py
+++ Point.py
@@ -1,5 +1,12 @@
 class Point:
-    ptX = 0
-    ptY = 0
     
+    def __init__(self,x_init,y_init):
+        self.x = x_init
+        self.y = y_init
     
+    def shift(self, x, y):
+        self.x += x
+        self.y += y
+        
+    def __repr__(self):
+        pass
