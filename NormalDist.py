99b061efba1b8e3cf4b4ace44bfefc3e4a00e659
jgrozell <joseph.rozell11@gmail.com>
2024-01-24 22:46:55 -0700

    just saving

--- NormalDist.py
+++ NormalDist.py
@@ -2,10 +2,13 @@ import numpy as np
 
 mu, sigma = 0, 0.1
 s = np.random.normal(mu, sigma, 1000)
+x = list(range(1,1001))
 
 import matplotlib.pyplot as plt
 count, bins, ignored = plt.hist(s,50,density=True)
 
 plt.plot(bins, 1/(sigma * np.sqrt(2*np.pi))*np.exp(-(bins-mu)**2/(2*sigma**2)), linewidth=2, color='r')
+plt.show()
 
+plt.plot(x,s)
 plt.show()
